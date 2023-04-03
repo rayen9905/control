dbc0eba01dad05f26027c4ed4158d6c8d0fe7ef1
rayen9905 <benromdhanrayen71@gmail.com>
2023-04-03 10:36:08 +0100

    commmit crossconfig

---
+++ src/main/java/com/pfe/CrossConfig/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.pfe.CrossConfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+    @Value("${allowed.origin}")
+    private String allowedOrigin;
+
+    @Bean
+    public WebMvcConfigurer getCorsConfiguration(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigin)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
